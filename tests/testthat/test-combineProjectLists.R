23dd9ac2786352f36d9615ff968fab58ef436422
Thilo Ammon <thiloammon@gmail.com>
2018-10-20 23:19:20 +0200

    Added README file

--- tests/testthat/test-combineProjectLists.R
+++ tests/testthat/test-combineProjectLists.R
@@ -5,6 +5,6 @@ test_that("combineProjectLists works",{
   projectA.tasks <- getTaskList(file.path(here::here(),"tests/testthat/Data/tasksForProjectA.csv"))
   projectB.tasks <- getTaskList(file.path(here::here(),"tests/testthat/Data/tasksForProjectB.csv"))
   combinedProjectList <- combineProjectLists(list(projectA.tasks,projectB.tasks),c("Project A","Project B"))
-  expect_equal(numberOfTasks(combinedProjectList),2)
-  expect_equal(combinedProjectList %>% pull(projectId),c("Project A","Project B"))
+  expect_equal(numberOfTasks(combinedProjectList),5)
+  expect_equal(combinedProjectList %>% pull(projectId) %>% unique(),c("Project A","Project B") )
 })
